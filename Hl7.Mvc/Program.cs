05f8d87694c0835aae8cd95b96ec23519a134f1c
Thomas Ngo <tngo0508@gmail.com>
2026-02-15 22:33:52 -0800

    Fix deployment: add runtime identifier, enable self-contained publishing, and configure forwarded headers

--- Hl7.Mvc/Program.cs
+++ Hl7.Mvc/Program.cs
@@ -1,3 +1,5 @@
+using Microsoft.AspNetCore.HttpOverrides;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -12,6 +14,11 @@ if (!app.Environment.IsDevelopment())
     // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
+
+app.UseForwardedHeaders(new ForwardedHeadersOptions
+{
+    ForwardedHeaders = ForwardedHeaders.XForwardedFor | ForwardedHeaders.XForwardedProto
+});
 
 app.UseHttpsRedirection();
 app.UseRouting();

c51ffe128132203b8b5e07e5c73238bc0c48da7d
Thomas Ngo <tngo0508@gmail.com>
2026-02-15 17:44:37 -0800

    Remove Privacy view and related references; update branding to CAIR2-HL7 in UI.

--- Hl7.Mvc/Controllers/HomeController.cs
+++ Hl7.Mvc/Controllers/HomeController.cs
@@ -42,11 +42,6 @@ public class HomeController : Controller
         return View(model);
     }
 
-    public IActionResult Privacy()
-    {
-        return View();
-    }
-
     [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
     public IActionResult Error()
     {
